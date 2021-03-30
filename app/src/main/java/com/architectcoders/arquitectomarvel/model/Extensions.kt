d0d93d68d5cf07504e9867baafcc0408912bcdf4
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-03-30 10:56:46 +0200

    feature_adapterList-characters Fix: Update loadUrl function

--- app/src/main/java/com/architectcoders/arquitectomarvel/model/Extensions.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/model/Extensions.kt
@@ -8,21 +8,23 @@ import com.bumptech.glide.Glide
 import java.math.BigInteger
 import java.security.MessageDigest
 
-val String.md5: String get()  {
-    val md = MessageDigest.getInstance("MD5")
-    return BigInteger(1, md.digest(this.toByteArray())).toString(16).padStart(32, '0')
-}
+val String.md5: String
+    get() {
+        val md = MessageDigest.getInstance("MD5")
+        return BigInteger(1, md.digest(this.toByteArray())).toString(16).padStart(32, '0')
+    }
 
 fun ImageView.loadUrl(url: String?, extension: String?) {
     Glide.with(this)
-            .asBitmap()
-            .load("$url/portrait_medium.$extension")
-            .fitCenter()
-            .error(R.drawable.marvel_error)
-            .into(this)
+        .asBitmap()
+        .load("$url.$extension")
+        .centerCrop()
+        .error(R.drawable.marvel_error)
+        .into(this)
 }
 
 fun RecyclerView.autoFitColumnsForGridLayout(columnWidthInDP: Float) {
-    val numberOfColumns: Int = ((resources.displayMetrics.widthPixels / resources.displayMetrics.density) / columnWidthInDP).toInt()
+    val numberOfColumns: Int =
+        ((resources.displayMetrics.widthPixels / resources.displayMetrics.density) / columnWidthInDP).toInt()
     layoutManager = GridLayoutManager(context, numberOfColumns)
 }
