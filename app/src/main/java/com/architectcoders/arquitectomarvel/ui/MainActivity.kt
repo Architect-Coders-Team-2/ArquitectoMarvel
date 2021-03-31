6484f40583ab4eea4bd09e76276b432822cd47bd
Jorge <representacionesgres@gmail.com>
2021-03-31 13:17:10 +0300

    Insert ProgressBar (close #17)

--- app/src/main/java/com/architectcoders/arquitectomarvel/ui/MainActivity.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/ui/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.architectcoders.arquitectomarvel.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.architectcoders.arquitectomarvel.BuildConfig
+import com.architectcoders.arquitectomarvel.BuildConfig.MARVEL_API_KEY
+import com.architectcoders.arquitectomarvel.BuildConfig.MARVEL_PRIVATE_KEY
 import com.architectcoders.arquitectomarvel.R
 import com.architectcoders.arquitectomarvel.databinding.ActivityMainBinding
 import com.architectcoders.arquitectomarvel.model.MarvelApiRest
@@ -25,17 +27,19 @@ class MainActivity : AppCompatActivity(), ClickListener {
         setContentView(binding.root)
 
         val ts = System.currentTimeMillis()
-        val publicKey = BuildConfig.MARVEL_API_KEY
-        val privateKey = BuildConfig.MARVEL_PRIVATE_KEY
+        val publicKey = MARVEL_API_KEY
+        val privateKey = MARVEL_PRIVATE_KEY
         val hash = "$ts$privateKey$publicKey".md5
         initRecyclerView()
 
         lifecycleScope.launchWhenResumed {
+            binding.progress.visibility = View.VISIBLE
             val characters = MarvelApiRest.service.getCharacters(ts, publicKey, hash)
             Timber.d("characters = $characters")
             Timber.d("characters.data?.results?.size ${characters.data?.results?.size}")
             (binding.mainHeroList.adapter as AdapterList).services =
                 characters.data?.results ?: emptyList()
+            binding.progress.visibility = View.GONE
         }
     }
 
