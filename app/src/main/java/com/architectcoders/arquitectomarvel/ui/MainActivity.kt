d3988c4dc4d8ea9d5c52d82a945de6cf87513a24
campi <joseantoniocampillo1112@gmail.com>
2021-04-01 11:32:11 +0200

    Update with rebase the current develop branch, for pull request

--- app/src/main/java/com/architectcoders/arquitectomarvel/ui/MainActivity.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/ui/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.architectcoders.arquitectomarvel.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.architectcoders.arquitectomarvel.BuildConfig
+import com.architectcoders.arquitectomarvel.BuildConfig.MARVEL_API_KEY
+import com.architectcoders.arquitectomarvel.BuildConfig.MARVEL_PRIVATE_KEY
 import com.architectcoders.arquitectomarvel.R
 import com.architectcoders.arquitectomarvel.databinding.ActivityMainBinding
 import com.architectcoders.arquitectomarvel.model.MarvelApiRest
 import com.architectcoders.arquitectomarvel.model.autoFitColumnsForGridLayout
 import com.architectcoders.arquitectomarvel.model.characters.Result
+import com.architectcoders.arquitectomarvel.model.database.ResultDatabase
+import com.architectcoders.arquitectomarvel.model.database.dbItemComics
+import com.architectcoders.arquitectomarvel.model.database.dbObject
+import com.architectcoders.arquitectomarvel.model.database.relations.ResultWithItemsComics
+import com.architectcoders.arquitectomarvel.model.database.relations.toListResult
 import com.architectcoders.arquitectomarvel.model.md5
 import com.architectcoders.arquitectomarvel.ui.main.AdapterList
 import com.architectcoders.arquitectomarvel.ui.main.ClickListener
@@ -34,10 +42,8 @@ class MainActivity : AppCompatActivity(), ClickListener {
         lifecycleScope.launchWhenResumed {
 
             binding.progress.visibility = View.VISIBLE
-            val characters = MarvelApiRest.service.getCharacters(ts, publicKey, hash)
-            val results = characters.data?.results ?: emptyList()
-            adapterList.submitList(results)
 
+            val characters = MarvelApiRest.service.getCharacters(ts, publicKey, hash)
             val resultsRemote = characters.data?.results?: emptyList()
 
             val dao = ResultDatabase.getInstance(this@MainActivity).resultDao
