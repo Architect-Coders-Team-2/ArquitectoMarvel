8983c7f86357eb088432fdfd52f90a5fc79306b0
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-04-08 16:00:08 +0200

    feature/detail-activity-addition Feat: Update the repository

--- app/src/main/java/com/architectcoders/arquitectomarvel/model/Repository.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/model/Repository.kt
@@ -5,7 +5,10 @@ import com.architectcoders.arquitectomarvel.BuildConfig
 import com.architectcoders.arquitectomarvel.model.characters.Characters
 import com.architectcoders.arquitectomarvel.model.characters.Result
 import com.architectcoders.arquitectomarvel.model.comics.Comic
-import com.architectcoders.arquitectomarvel.model.database.*
+import com.architectcoders.arquitectomarvel.model.database.DetailedComicEntity
+import com.architectcoders.arquitectomarvel.model.database.ResultDao
+import com.architectcoders.arquitectomarvel.model.database.ResultDatabase
+import com.architectcoders.arquitectomarvel.model.database.toCharacterEntity
 
 class Repository(private val activity: Activity) {
 
@@ -25,13 +28,24 @@ class Repository(private val activity: Activity) {
         return MarvelApiRest.service.getComics(characterId, ts, publicKey, hash)
     }
 
-    suspend fun insertFavoriteCharacters(favouriteCharacter: Result) {
-        dao.insertCharacters(favouriteCharacter.toCharacterEntity)
+    suspend fun insertFavoriteCharacter(favouriteCharacter: Result) {
+        dao.insertFavoriteCharacter(favouriteCharacter.toCharacterEntity)
     }
 
-    suspend fun insertComicFromFavoriteCharacter(comic: DetailedComicEntity) {
-        dao.insertDetailedComics(comic)
+    suspend fun insertFavoriteComic(comic: DetailedComicEntity) {
+        dao.insertFavoriteDetailedComic(comic)
     }
+
+    suspend fun deleteFavoriteCharacters(favouriteCharacter: Result) {
+        dao.deleteFavoriteCharacter(favouriteCharacter.toCharacterEntity)
+    }
+
+    suspend fun deleteFavoriteDetailedComic(comic: DetailedComicEntity) {
+        dao.deleteFavoriteDetailedComic(comic)
+    }
+
+    suspend fun isCharacterFavorite(characterId: Int): Boolean =
+        dao.isCharacterFavorite(characterId) != null
 
     val dao: ResultDao get() = ResultDatabase.getInstance(activity).resultDao
 }
