fdea73a65e6702f4760f7572951e360c3c74c968
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-04-13 22:01:13 +0200

    feature/detail-activity-MVVM-migration Feature: Updatel extensions file
    Add getViewModel extension function to retrieve the correct ViewModel

--- app/src/main/java/com/architectcoders/arquitectomarvel/model/Extensions.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/model/Extensions.kt
@@ -5,6 +5,10 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.annotation.DrawableRes
 import androidx.annotation.RawRes
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.architectcoders.arquitectomarvel.R
@@ -51,4 +55,12 @@ fun <T> Context.toast(msgResource: T, length: Int = Toast.LENGTH_SHORT) {
         is Int -> Toast.makeText(this, msgResource, length).show()
         is String -> Toast.makeText(this, msgResource, length).show()
     }
+}
+
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T : ViewModel> FragmentActivity.getViewModel(crossinline factory: () -> T): T {
+    val viewModelFactory = object : ViewModelProvider.Factory {
+        override fun <U : ViewModel> create(modelClass: Class<U>): U = factory() as U
+    }
+    return ViewModelProvider(this, viewModelFactory).get()
 }
