19f0ed7bf4619dc324408c2cfb3afd702b7ddd86
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-04-28 12:23:18 +0200

    feature/migrate-MainActivity-to-MVVM-Databinding Fix: Update MainViewModel
    Update MainViewModel to remove unnecessary functions. Now the selected view is passed on a function as a lambda

--- app/src/main/java/com/architectcoders/arquitectomarvel/ui/main/MainViewModel.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/ui/main/MainViewModel.kt
@@ -1,13 +1,11 @@
 package com.architectcoders.arquitectomarvel.ui.main
 
-import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.architectcoders.arquitectomarvel.model.Repository
 import com.architectcoders.arquitectomarvel.model.characters.Result
-import com.architectcoders.arquitectomarvel.ui.common.Event
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.net.UnknownHostException
@@ -22,12 +20,6 @@ class MainViewModel(private val repository: Repository) : ViewModel() {
 
     private val _errorMessage = MutableLiveData<String>()
     val errorMessage: LiveData<String> get() = _errorMessage
-
-    private val _navigation = MutableLiveData<Event<Result>>()
-    val navigation: LiveData<Event<Result>> = _navigation
-
-    private val _viewItem = MutableLiveData<View>()
-    val viewItem: LiveData<View> = _viewItem
 
     init {
         refresh()
@@ -46,10 +38,5 @@ class MainViewModel(private val repository: Repository) : ViewModel() {
                 _errorMessage.value = "No connection. Error DB"
             }
         }
-    }
-
-    fun onResultClick(result: Result, view: View) {
-        _viewItem.value = view
-        _navigation.value = Event(result)
     }
 }
