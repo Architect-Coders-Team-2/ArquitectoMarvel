3fd6e3f98e4a63559364f807935be851cd7007f4
Jorge <representacionesgres@gmail.com>
2021-05-05 14:19:15 +0300

    Feature/Remove unnecessary Models (close #37)

--- app/src/main/java/com/architectcoders/arquitectomarvel/model/characters/Result.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/model/characters/Result.kt
@@ -14,22 +14,12 @@ data class Result(
     val name: String? = null,
     @Json(name = "description")
     val description: String? = null,
-    @Json(name = "modified")
-    val modified: String? = null,
     @Json(name = "thumbnail")
     val thumbnail: Thumbnail? = null,
     @Json(name = "resourceURI")
     val resourceURI: String? = null,
     @Json(name = "comics")
-    val comics: Comics = Comics(),
-    @Json(name = "series")
-    val series: Series? = null,
-    @Json(name = "stories")
-    val stories: Stories? = null,
-    @Json(name = "events")
-    val events: Events? = null,
-    @Json(name = "urls")
-    val urls: List<Url>? = null
+    val comics: Comics = Comics()
 ) : Parcelable {
 
     // override for areItemsTheSame method run ok with equals operator '=='
@@ -42,14 +32,9 @@ data class Result(
         if (id != other.id) return false
         if (name != other.name) return false
         if (description != other.description) return false
-        if (modified != other.modified) return false
         if (thumbnail != other.thumbnail) return false
         if (resourceURI != other.resourceURI) return false
         if (comics != other.comics) return false
-        if (series != other.series) return false
-        if (stories != other.stories) return false
-        if (events != other.events) return false
-        if (urls != other.urls) return false
 
         return true
     }
@@ -58,14 +43,9 @@ data class Result(
         var result = id
         result = 31 * result + (name?.hashCode() ?: 0)
         result = 31 * result + (description?.hashCode() ?: 0)
-        result = 31 * result + (modified?.hashCode() ?: 0)
         result = 31 * result + (thumbnail?.hashCode() ?: 0)
         result = 31 * result + (resourceURI?.hashCode() ?: 0)
         result = 31 * result + comics.hashCode()
-        result = 31 * result + (series?.hashCode() ?: 0)
-        result = 31 * result + (stories?.hashCode() ?: 0)
-        result = 31 * result + (events?.hashCode() ?: 0)
-        result = 31 * result + (urls?.hashCode() ?: 0)
         return result
     }
 }
--- app/src/main/java/com/architectcoders/arquitectomarvel/model/comics/Comic.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/model/comics/Comic.kt
@@ -8,18 +8,8 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 @JsonClass(generateAdapter = true)
 data class Comic(
-    @Json(name = "attributionHTML")
-    val attributionHTML: String?,
-    @Json(name = "attributionText")
-    val attributionText: String?,
     @Json(name = "code")
     val code: Int?,
-    @Json(name = "copyright")
-    val copyright: String?,
     @Json(name = "data")
-    val comicData: Data?,
-    @Json(name = "etag")
-    val etag: String?,
-    @Json(name = "status")
-    val status: String?
+    val comicData: Data?
 ) : Parcelable
