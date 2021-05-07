6a4fcd860e53560babd5dc9aa4dac47be91ea358
campi <joseantoniocampillo1112@gmail.com>
2021-05-07 10:14:46 +0200

    feature/jose/clean First steps MarvelRepository and first useCases

---
+++ data/src/main/java/com/architectcoders/module/data/MarvelRepository.kt
@@ -0,0 +1,60 @@
+package com.architectcoders.module.data
+
+import com.architectcoders.module.domain.local_models.DetailedComic
+import com.architectcoders.module.domain.remote_models.Characters.Characters
+import com.architectcoders.module.domain.remote_models.Comics.Comics
+import com.architectcoders.module.domain.remote_models.Characters.Result as CharacterResult
+
+class MarvelRepository(
+    private val localDataSource: LocalDataSource,
+    private val remoteDataSource: RemoteDataSource,
+    private val credentialsApiRepository: CredentialsApiRepository
+) {
+
+    suspend fun getCharactersRemote(offset: Int): Characters {
+        return remoteDataSource.getCharacters(credentialsApiRepository, offset)
+    }
+
+    suspend fun getComicsFromCharacterRemote(characterId: Int): Comics {
+        return remoteDataSource.getComics(credentialsApiRepository, characterId)
+    }
+
+    suspend fun insertFavoriteCharacter(favouriteCharacter: CharacterResult) {
+        localDataSource.insertFavoriteCharacter(favouriteCharacter)
+    }
+
+    suspend fun insertFavoriteComic(comic: DetailedComic) {
+        localDataSource.insertFavoriteDetailedComic(comic)
+    }
+
+    suspend fun deleteFavoriteCharacters(favouriteCharacter: CharacterResult) {
+        localDataSource.deleteFavoriteCharacter(favouriteCharacter)
+    }
+
+    suspend fun deleteFavoriteDetailedComic(comic: DetailedComic) {
+        localDataSource.deleteFavoriteDetailedComic(comic)
+    }
+
+    suspend fun isCharacterFavorite(characterId: Int): Boolean =
+        localDataSource.isCharacterFavorite(characterId) != null
+}
+
+interface LocalDataSource {
+    suspend fun insertFavoriteCharacter(toCharacterEntity: CharacterResult)
+    suspend fun insertFavoriteDetailedComic(comic: DetailedComic)
+    suspend fun deleteFavoriteCharacter(favouriteCharacter: CharacterResult)
+    suspend fun deleteFavoriteDetailedComic(comic: DetailedComic)
+    suspend fun isCharacterFavorite(characterId: Int): Int?
+}
+
+interface RemoteDataSource {
+    suspend fun getCharacters(credentialsApiRepository: CredentialsApiRepository, offset: Int) : Characters
+    suspend fun getComics(credentialsApiRepository: CredentialsApiRepository, characterId: Int): Comics
+}
+
+interface CredentialsApiRepository {
+    val currentTime: Long
+    val publicKey: String
+    val privateKey: String
+    val hash: String
+}
---
+++ usescases/src/main/java/com/architectcoders/module/usescases/UseCaseGetCharactersRemote.kt
@@ -0,0 +1,11 @@
+package com.architectcoders.module.usescases
+
+import com.architectcoders.module.data.MarvelRepository
+import com.architectcoders.module.domain.remote_models.Characters.Characters as CharactersCharacters
+
+class UseCaseGetCharactersRemote(private val marvelRepository: MarvelRepository) {
+
+    suspend fun invoke(offset: Int): CharactersCharacters {
+        return marvelRepository.getCharactersRemote(offset)
+    }
+}
