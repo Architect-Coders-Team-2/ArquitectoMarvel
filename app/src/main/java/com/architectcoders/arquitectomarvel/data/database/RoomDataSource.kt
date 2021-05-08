4cebd50d4ebd5cf09db2543eaa512d44ae217e43
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-05-08 14:18:01 +0200

    feature/juan/clean Feat: Implement the LocalDataSource to manage Room

---
+++ app/src/main/java/com/architectcoders/arquitectomarvel/data/database/RoomDataSource.kt
@@ -0,0 +1,37 @@
+package com.architectcoders.arquitectomarvel.data.database
+
+import com.architectcoders.data.source.LocalDataSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import com.architectcoders.domain.characters.Result as CharacterResult
+import com.architectcoders.domain.comics.Result as ComicResult
+
+class RoomDataSource(db: CharacterDatabase) : LocalDataSource {
+
+    private val characterDao = db.characterDao
+
+    override suspend fun insertFavoriteCharacter(favouriteCharacter: CharacterResult) =
+        withContext(Dispatchers.IO) {
+            characterDao.insertFavoriteCharacter(favouriteCharacter.toCharacterEntity)
+        }
+
+    override suspend fun deleteFavoriteCharacter(favouriteCharacter: CharacterResult) =
+        withContext(Dispatchers.IO) {
+            characterDao.deleteFavoriteCharacter(favouriteCharacter.toCharacterEntity)
+        }
+
+    override suspend fun isCharacterFavorite(characterId: Int): Boolean =
+        withContext(Dispatchers.IO) {
+            characterDao.isCharacterFavorite(characterId) != null
+        }
+
+    override suspend fun insertFavoriteDetailedComic(comic: ComicResult) =
+        withContext(Dispatchers.IO) {
+            characterDao.insertFavoriteComic(comic.toComicEntity)
+        }
+
+    override suspend fun deleteFavoriteDetailedComic(comic: ComicResult) =
+        withContext(Dispatchers.IO) {
+            characterDao.deleteFavoriteComic(comic.toComicEntity)
+        }
+}
