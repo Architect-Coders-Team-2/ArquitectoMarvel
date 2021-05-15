1b59e28cd73d5211e9d63abf6e5a7c219fad02fc
Jorge <representacionesgres@gmail.com>
2021-05-15 20:32:24 +0300

    Module Domain Ready

---
+++ domain/src/main/java/com/architectcoders/domain/characters/Comics.kt
@@ -0,0 +1,14 @@
+package com.architectcoders.domain.characters
+
+data class Comics(
+    val available: Int? = null,
+    val collectionURI: String = "",
+    val items: List<Item> = emptyList(),
+    val returned: Int? = null
+)
+
+{
+    override fun toString(): String {
+        return "Comics(available=$available, collectionURI=$collectionURI, items=$items, returned=$returned)"
+    }
+}
---
+++ domain/src/main/java/com/architectcoders/domain/comics/Result.kt
@@ -0,0 +1,13 @@
+package com.architectcoders.domain.comics
+
+import com.architectcoders.domain.characters.Thumbnail
+
+data class Result(
+    val description: String?,
+    val id: Int?,
+    val images: List<Image>?,
+    val pageCount: Int?,
+    val resourceURI: String?,
+    val thumbnail: Thumbnail?,
+    val title: String?
+)
