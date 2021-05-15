a73768fff3e3769607c443dcedea256505663ddb
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-05-15 16:25:21 +0200

    feature/juan/di-hilt Feat: Init Hilt

--- app/src/main/java/com/architectcoders/arquitectomarvel/App.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/App.kt
@@ -2,10 +2,12 @@ package com.architectcoders.arquitectomarvel
 
 import android.app.Application
 import android.util.Log
+import dagger.hilt.android.HiltAndroidApp
 import org.jetbrains.annotations.NotNull
 import timber.log.Timber
 
-class App: Application() {
+@HiltAndroidApp
+class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
@@ -17,21 +19,13 @@ class App: Application() {
     }
 }
 
-/** todo: Consultar con el equipo. Eliminar si no se considera
- * Personalizamos los eventos de Timber para la release build de la app
- * De forma que podemos reutilizar estas llamadas para monitorear desde crashlitics u otro sistema
- */
 class ReleaseTree : @NotNull Timber.Tree() {
 
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
         if (priority == Log.ERROR) {
-//            crashlytics().setCustomKey(
-//                "Log.ERROR", message
-//            )
+            // TODO
         } else if (priority == Log.WARN) {
-//            crashlytics().setCustomKey(
-//                "Log.WARN", message
-//            )
+            // TODO
         }
     }
 }
