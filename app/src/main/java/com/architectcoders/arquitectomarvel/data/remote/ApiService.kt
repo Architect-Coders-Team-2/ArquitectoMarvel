76a41b8f7ef49cd1491056ea5ad41142578d48df
campi <joseantoniocampillo1112@gmail.com>
2021-05-18 18:50:43 +0200

    feature/jose/clean Fix paging bug repeating heros

--- app/src/main/java/com/architectcoders/arquitectomarvel/data/remote/ApiService.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/data/remote/ApiService.kt
@@ -2,12 +2,12 @@ package com.architectcoders.arquitectomarvel.data.remote
 
 import com.architectcoders.arquitectomarvel.data.remote.models_moshi.characters.Characters
 import com.architectcoders.arquitectomarvel.data.remote.models_moshi.comics.Comic
+import com.architectcoders.arquitectomarvel.ui.common.INCREMENT
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 const val BASE_URL = "https://gateway.marvel.com"
-const val LIMIT = 90
 
 interface ApiService {
 
@@ -17,7 +17,7 @@ interface ApiService {
         @Query("apikey") apikey: String,
         @Query("hash") hash: String,
         @Query("offset") offset: Int,
-        @Query("limit") limit: Int = LIMIT
+        @Query("limit") limit: Int = INCREMENT
     ): Characters
 
     @GET("/v1/public/characters/{characterId}/comics")
