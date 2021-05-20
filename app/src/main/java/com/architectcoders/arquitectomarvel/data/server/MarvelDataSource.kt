7093fde2161ab437a4ed839595429dc2c3485846
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-05-20 17:19:46 +0200

    feature/04-clean-architecture Feat: Implemented the remote data source
    The credential is added as a paramenter to pass the necessary data and casted the generic types

--- app/src/main/java/com/architectcoders/arquitectomarvel/data/server/MarvelDataSource.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/data/server/MarvelDataSource.kt
@@ -2,32 +2,38 @@ package com.architectcoders.arquitectomarvel.data.server
 
 import com.architectcoders.arquitectomarvel.data.server.uiEntities.marvelCharacters.toLocalCharacters
 import com.architectcoders.arquitectomarvel.data.server.uiEntities.marvelComics.toLocalComic
+import com.architectcoders.data.source.CredentialsDataSource
 import com.architectcoders.data.source.RemoteDataSource
-import com.architectcoders.domain.characters.Characters
-import com.architectcoders.domain.comics.Comic
 
-class MarvelDataSource : RemoteDataSource {
-    override suspend fun getCharacters(
-        ts: Long,
-        publicKey: String,
-        hash: String,
-        offset: Int
-    ): Characters =
-        MarvelApiRest.service.getCharacters(ts, publicKey, hash, offset).toLocalCharacters
+class MarvelDataSource(override val credentialsDataSource: CredentialsDataSource) :
+    RemoteDataSource {
 
-    override suspend fun getCharacterById(
-        characterId: Int,
-        ts: Long,
-        publicKey: String,
-        hash: String
-    ): Characters =
-        MarvelApiRest.service.getCharacterById(characterId, ts, publicKey, hash).toLocalCharacters
+    private val marvelApi = MarvelApiRest.service
 
-    override suspend fun getComics(
-        characterId: Int,
-        ts: Long,
-        publicKey: String,
-        hash: String
-    ): Comic? =
-        MarvelApiRest.service.getComics(characterId, ts, publicKey, hash)?.toLocalComic
+    private val marvelCredentialDataSource: MarvelCredentialDataSource =
+        credentialsDataSource as MarvelCredentialDataSource
+
+    override suspend fun getRemoteCharacters(vararg param: Any): Any =
+        marvelApi.getCharacters(
+            marvelCredentialDataSource.timeStamp,
+            marvelCredentialDataSource.publicKey,
+            marvelCredentialDataSource.hash,
+            param.first() as Int
+        ).toLocalCharacters
+
+    override suspend fun getRemoteCharacterById(vararg param: Any): Any =
+        marvelApi.getCharacterById(
+            param.first() as Int,
+            marvelCredentialDataSource.timeStamp,
+            marvelCredentialDataSource.publicKey,
+            marvelCredentialDataSource.hash
+        ).toLocalCharacters
+
+    override suspend fun getRemoteComics(vararg param: Any): Any? =
+        marvelApi.getComics(
+            param.first() as Int,
+            marvelCredentialDataSource.timeStamp,
+            marvelCredentialDataSource.publicKey,
+            marvelCredentialDataSource.hash
+        )?.toLocalComic
 }
