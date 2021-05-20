757d3bad87519528e9d33eedfad13d780db8fcfb
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-05-20 17:29:45 +0200

    feature/04-clean-architecture Feat: Update the comic adapter
    The comic adapter has been updated with the domain data

--- app/src/main/java/com/architectcoders/arquitectomarvel/ui/detail/ComicAdapter.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/ui/detail/ComicAdapter.kt
@@ -4,39 +4,27 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
-import com.architectcoders.arquitectomarvel.R
 import com.architectcoders.arquitectomarvel.databinding.ComicItemBinding
-import com.architectcoders.arquitectomarvel.data.database.ComicEntity
-import com.architectcoders.arquitectomarvel.ui.common.loadUrl
+import com.architectcoders.domain.comics.Result as ComicResult
 
-class ComicAdapter : ListAdapter<ComicEntity, ComicAdapter.ViewHolder>(DiffUtilCallback) {
+class ComicAdapter : ListAdapter<ComicResult, ComicViewHolder>(DiffUtilCallback) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ComicViewHolder {
         val binding =
             ComicItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+        return ComicViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ComicViewHolder, position: Int) {
         val detailedComicEntity = getItem(position)
         holder.bind(detailedComicEntity)
     }
-
-    class ViewHolder(private val binding: ComicItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun bind(comicEntity: ComicEntity) {
-            binding.comicImage.loadUrl(comicEntity.thumbnail)
-            binding.comicTitle.text =
-                comicEntity.title ?: binding.root.context.getString(R.string.content_not_available)
-        }
-    }
 }
 
-private object DiffUtilCallback : DiffUtil.ItemCallback<ComicEntity>() {
-    override fun areItemsTheSame(oldItem: ComicEntity, newItem: ComicEntity): Boolean =
-        oldItem.resourceUri == newItem.resourceUri
+private object DiffUtilCallback : DiffUtil.ItemCallback<ComicResult>() {
+    override fun areItemsTheSame(oldItem: ComicResult, newItem: ComicResult): Boolean =
+        oldItem.resourceURI == newItem.resourceURI
 
-    override fun areContentsTheSame(oldItem: ComicEntity, newItem: ComicEntity): Boolean =
+    override fun areContentsTheSame(oldItem: ComicResult, newItem: ComicResult): Boolean =
         oldItem == newItem
 }
