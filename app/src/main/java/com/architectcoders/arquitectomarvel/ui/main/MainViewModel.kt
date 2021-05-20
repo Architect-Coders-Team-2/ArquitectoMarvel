1a2895c00b8f7bdd4e040688633957c8bf6c9ef2
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-05-20 17:27:15 +0200

    feature/04-clean-architecture Refactor: Update MainViewModel
    Updated with the proper function and constant names

--- app/src/main/java/com/architectcoders/arquitectomarvel/ui/main/MainViewModel.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/ui/main/MainViewModel.kt
@@ -5,13 +5,13 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
+import com.architectcoders.arquitectomarvel.ui.common.REQUEST_LIMIT
 import com.architectcoders.arquitectomarvel.ui.main.pagination.ResultPagingSource
-import com.architectcoders.data.repository.CharacterRepository
-import com.architectcoders.usecases.GetCharacters
+import com.architectcoders.usecases.GetRemoteCharacters
 
-class MainViewModel(private val getCharacters: GetCharacters) : ViewModel() {
+class MainViewModel(private val getRemoteCharacters: GetRemoteCharacters) : ViewModel() {
     val pager = Pager(
-        config = PagingConfig(pageSize = 18),
-        pagingSourceFactory = { ResultPagingSource(getCharacters) }
+        config = PagingConfig(pageSize = REQUEST_LIMIT / 2),
+        pagingSourceFactory = { ResultPagingSource(getRemoteCharacters) }
     ).flow.cachedIn(viewModelScope)
 }
