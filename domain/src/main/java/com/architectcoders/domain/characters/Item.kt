bfcb39ee3f6294fb656fd1622f241aed947ada13
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-05-20 16:43:54 +0200

    feature/04-clean-architecture Refactor: Remove unused code from Ch. Item

--- domain/src/main/java/com/architectcoders/domain/characters/Item.kt
+++ domain/src/main/java/com/architectcoders/domain/characters/Item.kt
@@ -3,8 +3,4 @@ package com.architectcoders.domain.characters
 data class Item(
     val resourceURI: String?,
     val name: String?
-) {
-    override fun toString(): String {
-        return "Item(resourceURI=$resourceURI, name=$name)"
-    }
-}
+)
