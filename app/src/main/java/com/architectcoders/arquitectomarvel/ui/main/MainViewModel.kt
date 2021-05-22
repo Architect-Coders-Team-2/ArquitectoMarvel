33b3e94dcaad913482b35e3b4a4ede4cb1785636
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-05-22 22:32:23 +0200

    feature/remoteMediator Feat: Create the Pager for the RemoteMediator

--- app/src/main/java/com/architectcoders/arquitectomarvel/ui/main/MainViewModel.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/ui/main/MainViewModel.kt
@@ -2,16 +2,32 @@ package com.architectcoders.arquitectomarvel.ui.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.cachedIn
+import androidx.paging.*
+import com.architectcoders.arquitectomarvel.data.database.CharacterEntity
 import com.architectcoders.arquitectomarvel.ui.common.REQUEST_LIMIT
-import com.architectcoders.arquitectomarvel.ui.main.pagination.ResultPagingSource
-import com.architectcoders.usecases.GetRemoteCharacters
+import com.architectcoders.arquitectomarvel.ui.main.pagination.CharacterRemoteMediator
+import com.architectcoders.usecases.*
 
-class MainViewModel(private val getRemoteCharacters: GetRemoteCharacters) : ViewModel() {
+class MainViewModel(
+    getRemoteCharacters: GetRemoteCharacters,
+    deleteAllCharacters: DeleteAllCharacters,
+    insertAllCharacters: InsertAllCharacters,
+    getLastTimeStamp: GetLastTimeStamp,
+    getPagingSource: GetPagingSource,
+    getStoredCharactersCount: GetStoredCharactersCount
+) : ViewModel() {
+    @ExperimentalPagingApi
     val pager = Pager(
         config = PagingConfig(pageSize = REQUEST_LIMIT / 2),
-        pagingSourceFactory = { ResultPagingSource(getRemoteCharacters) }
-    ).flow.cachedIn(viewModelScope)
+        remoteMediator =
+        CharacterRemoteMediator(
+            getRemoteCharacters,
+            deleteAllCharacters,
+            insertAllCharacters,
+            getLastTimeStamp,
+            getStoredCharactersCount
+        )
+    ) {
+        getPagingSource.invoke() as PagingSource<Int, CharacterEntity>
+    }.flow.cachedIn(viewModelScope)
 }
