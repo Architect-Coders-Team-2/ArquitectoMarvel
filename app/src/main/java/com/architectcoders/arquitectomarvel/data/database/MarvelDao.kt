04ba1ca5d5dee956b6409256299e5f96f933490c
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-05-22 22:19:57 +0200

    feature/remoteMediator Feat: Implement the functions for RemoteMediator
    And changed any character by id should be loaded o
    in the DB

--- app/src/main/java/com/architectcoders/arquitectomarvel/data/database/MarvelDao.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/data/database/MarvelDao.kt
@@ -1,5 +1,6 @@
 package com.architectcoders.arquitectomarvel.data.database
 
+import androidx.paging.PagingSource
 import androidx.room.*
 import com.architectcoders.arquitectomarvel.data.database.relations.CharacterWithComics
 
@@ -7,24 +8,42 @@ import com.architectcoders.arquitectomarvel.data.database.relations.CharacterWit
 interface MarvelDao {
 
     @Query("SELECT * FROM characterentity ORDER BY name")
-    suspend fun getLocalCharacters(): List<CharacterEntity>
+    suspend fun getLocalCharacters(): List<CharacterEntity>?
+
+    @Query("SELECT * FROM characterentity WHERE id = :characterId")
+    suspend fun getLocalCharacterById(characterId: Int): CharacterEntity
+
+    @Query("SELECT insertDate FROM characterentity LIMIT 1")
+    suspend fun getLastTimeStamp(): Long?
+
+    @Query("SELECT COUNT(id) FROM characterentity")
+    suspend fun getStoredCharactersCount(): Int?
 
     @Transaction
     @Query("SELECT * FROM characterentity WHERE comicCollectionUri = :comicCollectionUri")
     suspend fun getCharacterWithComics(comicCollectionUri: String): List<CharacterWithComics>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertFavoriteCharacter(characterEntity: CharacterEntity)
+    suspend fun insertAllCharacters(characterList: List<CharacterEntity>)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertFavoriteComic(comicEntity: ComicEntity)
+    suspend fun insertFavoriteCharacter(favoriteCharacterEntity: FavoriteCharacterEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteComic(favoriteComicEntity: FavoriteComicEntity)
 
     @Delete
-    suspend fun deleteFavoriteCharacter(characterEntity: CharacterEntity)
+    suspend fun deleteFavoriteCharacter(favoriteCharacterEntity: FavoriteCharacterEntity)
 
     @Delete
-    suspend fun deleteFavoriteComic(comicEntity: ComicEntity)
+    suspend fun deleteFavoriteComic(favoriteComicEntity: FavoriteComicEntity)
 
-    @Query("SELECT id FROM characterentity WHERE id = :id")
+    @Query("SELECT id FROM favoritecharacterentity WHERE id = :id")
     suspend fun isCharacterFavorite(id: Int): Int?
+
+    @Query("SELECT * FROM characterentity ORDER BY name")
+    fun getPagingSource(): PagingSource<Int, CharacterEntity>
+
+    @Query("DELETE FROM characterentity")
+    suspend fun deleteAllCharacters()
 }
--- app/src/main/java/com/architectcoders/arquitectomarvel/data/database/RoomDataSource.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/data/database/RoomDataSource.kt
@@ -1,5 +1,6 @@
 package com.architectcoders.arquitectomarvel.data.database
 
+import androidx.paging.PagingSource
 import com.architectcoders.data.source.LocalDataSource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -10,19 +11,44 @@ class RoomDataSource(db: MarvelDatabase) : LocalDataSource {
 
     private val characterDao = db.marvelDao
 
-    override suspend fun getLocalCharacters(): List<CharacterResult> =
+    override suspend fun getLocalCharacters(): List<CharacterResult>? =
         withContext(Dispatchers.IO) {
-            characterDao.getLocalCharacters().toDomainCharacterList
+            characterDao.getLocalCharacters()?.toDomainCharacterList
+        }
+
+    override suspend fun getLocalCharacterById(characterId: Int): CharacterResult =
+        withContext(Dispatchers.IO) {
+            characterDao.getLocalCharacterById(characterId).toDomainCharacter
+        }
+
+    override suspend fun getLastTimeStamp(): Long? =
+        withContext(Dispatchers.IO) {
+            characterDao.getLastTimeStamp()
+        }
+
+    override suspend fun getStoredCharactersCount(): Int =
+        withContext(Dispatchers.IO) {
+            characterDao.getStoredCharactersCount() ?: 0
+        }
+
+    override suspend fun insertAllCharacters(characterList: List<com.architectcoders.domain.characters.Result>) =
+        withContext(Dispatchers.IO) {
+            characterDao.insertAllCharacters(characterList.toCharacterEntityList)
+        }
+
+    override suspend fun deleteAllCharacters() =
+        withContext(Dispatchers.IO) {
+            characterDao.deleteAllCharacters()
         }
 
     override suspend fun insertFavoriteCharacter(favoriteCharacter: CharacterResult) =
         withContext(Dispatchers.IO) {
-            characterDao.insertFavoriteCharacter(favoriteCharacter.toCharacterEntity)
+            characterDao.insertFavoriteCharacter(favoriteCharacter.toFavoriteCharacterEntity)
         }
 
     override suspend fun deleteFavoriteCharacter(favoriteCharacter: CharacterResult) =
         withContext(Dispatchers.IO) {
-            characterDao.deleteFavoriteCharacter(favoriteCharacter.toCharacterEntity)
+            characterDao.deleteFavoriteCharacter(favoriteCharacter.toFavoriteCharacterEntity)
         }
 
     override suspend fun isCharacterFavorite(characterId: Int): Boolean =
@@ -32,11 +58,14 @@ class RoomDataSource(db: MarvelDatabase) : LocalDataSource {
 
     override suspend fun insertFavoriteDetailedComic(favoriteComic: ComicResult) =
         withContext(Dispatchers.IO) {
-            characterDao.insertFavoriteComic(favoriteComic.toComicEntity)
+            characterDao.insertFavoriteComic(favoriteComic.toFavoriteComicEntity)
         }
 
     override suspend fun deleteFavoriteDetailedComic(favoriteComic: ComicResult) =
         withContext(Dispatchers.IO) {
-            characterDao.deleteFavoriteComic(favoriteComic.toComicEntity)
+            characterDao.deleteFavoriteComic(favoriteComic.toFavoriteComicEntity)
         }
+
+    override fun getPagingSource(): PagingSource<Int, CharacterEntity> =
+        characterDao.getPagingSource()
 }
