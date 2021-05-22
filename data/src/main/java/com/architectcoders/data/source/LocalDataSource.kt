47ae705a6a2e6ead48388ce22bd78acedc389dc9
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-05-22 21:56:51 +0200

    feature/remoteMediator Feat: Update the LocalDataSource
    These funcions are needed for the use cases

--- data/src/main/java/com/architectcoders/data/source/LocalDataSource.kt
+++ data/src/main/java/com/architectcoders/data/source/LocalDataSource.kt
@@ -1,13 +1,19 @@
 package com.architectcoders.data.source
 
-import com.architectcoders.domain.comics.Result as ComicResult
 import com.architectcoders.domain.characters.Result as CharacterResult
+import com.architectcoders.domain.comics.Result as ComicResult
 
 interface LocalDataSource {
     suspend fun getLocalCharacters(): List<CharacterResult>?
+    suspend fun getLocalCharacterById(characterId: Int): CharacterResult
+    suspend fun getLastTimeStamp(): Long?
+    suspend fun getStoredCharactersCount(): Int
     suspend fun isCharacterFavorite(characterId: Int): Boolean
+    suspend fun insertAllCharacters(characterList: List<CharacterResult>)
+    suspend fun deleteAllCharacters()
     suspend fun insertFavoriteCharacter(favoriteCharacter: CharacterResult)
     suspend fun deleteFavoriteCharacter(favoriteCharacter: CharacterResult)
     suspend fun insertFavoriteDetailedComic(favoriteComic: ComicResult)
     suspend fun deleteFavoriteDetailedComic(favoriteComic: ComicResult)
+    fun getPagingSource(): Any?
 }
