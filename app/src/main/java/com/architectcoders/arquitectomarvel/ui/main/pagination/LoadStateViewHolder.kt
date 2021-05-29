6917646777d3169ae1917fa7ef314fc720d15fd1
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-05-29 17:40:33 +0200

    feature/internet-connection-checker Feat: Update LoadStateViewHolder
    Updated to show user-friendly messages when the app fails to display characters

--- app/src/main/java/com/architectcoders/arquitectomarvel/ui/main/pagination/LoadStateViewHolder.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/ui/main/pagination/LoadStateViewHolder.kt
@@ -3,6 +3,7 @@ package com.architectcoders.arquitectomarvel.ui.main.pagination
 import androidx.core.view.isVisible
 import androidx.paging.LoadState
 import androidx.recyclerview.widget.RecyclerView
+import com.architectcoders.arquitectomarvel.R
 import com.architectcoders.arquitectomarvel.databinding.ReposLoadStateFooterViewItemBinding
 
 class LoadStateViewHolder(
@@ -12,7 +13,7 @@ class LoadStateViewHolder(
 
     fun bind(loadState: LoadState) {
         if (loadState is LoadState.Error) {
-            binding.errorMsg.text = loadState.error.localizedMessage
+            binding.errorMsg.text = binding.root.context.getString(R.string.no_internet)
         }
         binding.errorMsg.isVisible = loadState is LoadState.Error
         binding.progressBar.isVisible = loadState is LoadState.Loading
