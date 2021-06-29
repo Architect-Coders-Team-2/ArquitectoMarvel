03100e0a105dddbd4f7b2a207ed1b75b5390e644
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-06-29 20:05:21 +0200

    feature/hilt Fix:  Remove the unnecessary Singleton tags
    This is done to avoid to mix scopes

--- app/src/main/java/com/architectcoders/arquitectomarvel/ui/main/di/MainViewModelModule.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/ui/main/di/MainViewModelModule.kt
@@ -10,34 +10,28 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(ViewModelComponent::class)
-class MainViewModelDi {
+class MainViewModelModule {
 
-    @Singleton
     @Provides
     fun getRemoteCharactersProvider(marvelRepository: MarvelRepository): GetRemoteCharacters =
         GetRemoteCharacters(marvelRepository)
 
-    @Singleton
     @Provides
     fun deleteAllLocalCharactersProvider(marvelRepository: MarvelRepository): DeleteAllLocalCharacters =
         DeleteAllLocalCharacters(marvelRepository)
 
-    @Singleton
     @Provides
     fun insertAllLocalCharactersProvider(marvelRepository: MarvelRepository): InsertAllLocalCharacters =
         InsertAllLocalCharacters(marvelRepository)
 
-    @Singleton
     @Provides
     fun getLastTimeStampFromCharacterEntityProvider(marvelRepository: MarvelRepository): GetLastTimeStampFromCharacterEntity =
         GetLastTimeStampFromCharacterEntity(marvelRepository)
 
-    @Singleton
     @Provides
     fun getPagingSourceFromCharacterEntityProvider(marvelRepository: MarvelRepository): GetPagingSourceFromCharacterEntity =
         GetPagingSourceFromCharacterEntity(marvelRepository)
 
-    @Singleton
     @Provides
     fun getLocalCharactersCountProvider(marvelRepository: MarvelRepository): GetLocalCharactersCount =
         GetLocalCharactersCount(marvelRepository)
