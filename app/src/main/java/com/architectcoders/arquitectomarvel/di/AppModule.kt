55f4adc32b6f56802b7a29048ef91ae54a33a1d4
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-06-29 17:58:45 +0200

    feature/hilt Fix: Update AppModule

--- app/src/main/java/com/architectcoders/arquitectomarvel/di/AppModule.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/di/AppModule.kt
@@ -1,19 +1,14 @@
 package com.architectcoders.arquitectomarvel.di
 
 import android.app.Application
-import android.content.Context
 import androidx.room.Room
-import com.architectcoders.arquitectomarvel.biometric.BiometricDataSourceImpl
 import com.architectcoders.arquitectomarvel.data.database.MarvelDatabase
 import com.architectcoders.arquitectomarvel.data.database.RoomDataSource
 import com.architectcoders.arquitectomarvel.data.server.MarvelCredentialDataSource
 import com.architectcoders.arquitectomarvel.data.server.RetrofitDataSource
-import com.architectcoders.arquitectomarvel.network.NetworkDataSourceImpl
-import com.architectcoders.data.source.CredentialsDataSource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ActivityContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -36,9 +31,6 @@ class AppModule {
 
     @Singleton
     @Provides
-    fun retrofitDataSourceProvider(): RetrofitDataSource = RetrofitDataSource(MarvelCredentialDataSource())
-
-    @Singleton
-    @Provides
-    fun networkDataSourceProvider(app: Application): NetworkDataSourceImpl = NetworkDataSourceImpl(app)
+    fun retrofitDataSourceProvider(): RetrofitDataSource =
+        RetrofitDataSource(MarvelCredentialDataSource())
 }
