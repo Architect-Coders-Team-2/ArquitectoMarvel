fcbe3756e6c018b01973462242f1fa0d9ff333ad
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-07-20 18:48:16 +0200

    feature/hilt-improvement Fix: Update DataModule to bind datasources

--- app/src/main/java/com/architectcoders/arquitectomarvel/di/DataModule.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/di/DataModule.kt
@@ -1,10 +1,16 @@
 package com.architectcoders.arquitectomarvel.di
 
 import com.architectcoders.arquitectomarvel.data.database.RoomDataSource
+import com.architectcoders.arquitectomarvel.data.server.MarvelCredentialDataSource
 import com.architectcoders.arquitectomarvel.data.server.RetrofitDataSource
 import com.architectcoders.arquitectomarvel.network.NetworkDataSourceImpl
 import com.architectcoders.data.repository.MarvelRepository
 import com.architectcoders.data.repository.NetworkRepository
+import com.architectcoders.data.source.CredentialsDataSource
+import com.architectcoders.data.source.LocalDataSource
+import com.architectcoders.data.source.NetworkDataSource
+import com.architectcoders.data.source.RemoteDataSource
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,7 +19,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class DataModule {
+class DataModuleProvider {
 
     @Singleton
     @Provides
@@ -26,4 +32,25 @@ class DataModule {
     @Provides
     fun networkRepositoryProvider(networkDataSourceImpl: NetworkDataSourceImpl): NetworkRepository =
         NetworkRepository(networkDataSourceImpl)
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataModuleBinder {
+
+    @Singleton
+    @Binds
+    abstract fun bindsRoomDataSource(roomDataSource: RoomDataSource): LocalDataSource
+
+    @Singleton
+    @Binds
+    abstract fun bindsCredentialDataSource(marvelCredentialDataSource: MarvelCredentialDataSource): CredentialsDataSource
+
+    @Singleton
+    @Binds
+    abstract fun bindsRetrofitDataSource(retrofitDataSource: RetrofitDataSource): RemoteDataSource
+
+    @Singleton
+    @Binds
+    abstract fun bindsNetworkDataSource(networkDataSourceImpl: NetworkDataSourceImpl): NetworkDataSource
 }
