e169476e33dd4761f2b89fd7b02c0b809f8d4b39
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-08-12 13:49:34 +0200

    feature/improve-biometric-logic Feat: Update the BiometricRepository
    It has been updated to manage all the use cases

--- data/src/main/java/com/architectcoders/data/repository/BiometricRepository.kt
+++ data/src/main/java/com/architectcoders/data/repository/BiometricRepository.kt
@@ -1,9 +1,25 @@
 package com.architectcoders.data.repository
 
-import com.architectcoders.data.source.BiometricDataSource
+import com.architectcoders.data.source.BiometricPromptDataSource
+import com.architectcoders.data.source.AuthenticationStateDataSource
+import com.architectcoders.data.source.LocalDataSource
 
-class BiometricRepository(private val biometricDataSource: BiometricDataSource) {
+class BiometricRepository(
+    private val authenticationStateDataSource: AuthenticationStateDataSource,
+    private val biometricPromptDataSource: BiometricPromptDataSource
+) {
+    fun checkAuthenticationState(): Unit =
+        authenticationStateDataSource.checkAuthenticationState()
+
+    fun checkIfUserIsAlreadyAuthenticated(): Boolean =
+        authenticationStateDataSource.checkIfUserIsAlreadyAuthenticated()
+
+    fun saveAuthenticationState(isAuthenticated: Boolean): Unit =
+        authenticationStateDataSource.saveAuthenticationState(isAuthenticated)
+
+    fun canUserUseBiometricAuthentication(): Boolean =
+        biometricPromptDataSource.canUserUseBiometricAuthentication()
 
     fun setBiometricAuthentication(onFail: () -> Unit, onSuccess: () -> Unit): Unit =
-        biometricDataSource.setBiometricAuthentication(onFail, onSuccess)
+        biometricPromptDataSource.setBiometricAuthentication(onFail, onSuccess)
 }
