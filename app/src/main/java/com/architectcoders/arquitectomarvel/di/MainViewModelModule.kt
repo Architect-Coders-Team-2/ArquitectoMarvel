f16a52dcb781a7084667adea1caa7fea834a6ccb
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-08-12 19:49:12 +0200

    feature/improve-biometric-logic Fix: Retrieve MainViewModelModule as it was

---
+++ app/src/main/java/com/architectcoders/arquitectomarvel/di/MainViewModelModule.kt
@@ -0,0 +1,71 @@
+package com.architectcoders.arquitectomarvel.di
+
+import androidx.paging.ExperimentalPagingApi
+import com.architectcoders.arquitectomarvel.ui.main.pagination.CharacterRemoteMediator
+import com.architectcoders.data.repository.BiometricRepository
+import com.architectcoders.data.repository.MarvelRepository
+import com.architectcoders.usecases.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class MainViewModelModule {
+
+    @ViewModelScoped
+    @Provides
+    fun getPagingSourceFromCharacterEntityProvider(
+        marvelRepository: MarvelRepository
+    ): GetPagingSourceFromCharacterEntity =
+        GetPagingSourceFromCharacterEntity(marvelRepository)
+
+    // CharacterRemoteMediator
+
+    @ViewModelScoped
+    @Provides
+    fun getRemoteCharactersProvider(marvelRepository: MarvelRepository): GetRemoteCharacters =
+        GetRemoteCharacters(marvelRepository)
+
+    @ViewModelScoped
+    @Provides
+    fun deleteAllLocalCharactersProvider(marvelRepository: MarvelRepository): DeleteAllLocalCharacters =
+        DeleteAllLocalCharacters(marvelRepository)
+
+    @ViewModelScoped
+    @Provides
+    fun insertAllLocalCharactersProvider(marvelRepository: MarvelRepository): InsertAllLocalCharacters =
+        InsertAllLocalCharacters(marvelRepository)
+
+    @ViewModelScoped
+    @Provides
+    fun getLastTimeStampFromCharacterEntityProvider(
+        marvelRepository: MarvelRepository
+    ): GetLastTimeStampFromCharacterEntity =
+        GetLastTimeStampFromCharacterEntity(marvelRepository)
+
+    @ViewModelScoped
+    @Provides
+    fun getLocalCharactersCountProvider(marvelRepository: MarvelRepository): GetLocalCharactersCount =
+        GetLocalCharactersCount(marvelRepository)
+
+    @ExperimentalPagingApi
+    @ViewModelScoped
+    @Provides
+    fun getCharacterRemoteMediatorProvider(
+        getRemoteCharacters: GetRemoteCharacters,
+        deleteAllLocalCharacters: DeleteAllLocalCharacters,
+        insertAllLocalCharacters: InsertAllLocalCharacters,
+        getLastTimeStampFromCharacterEntity: GetLastTimeStampFromCharacterEntity,
+        getLocalCharactersCount: GetLocalCharactersCount
+    ): CharacterRemoteMediator =
+        CharacterRemoteMediator(
+            getRemoteCharacters,
+            deleteAllLocalCharacters,
+            insertAllLocalCharacters,
+            getLastTimeStampFromCharacterEntity,
+            getLocalCharactersCount
+        )
+}
