286d80fa17abdc4f659d3377d891b8c5c0f7acb7
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-08-15 16:53:43 +0200

    feature/no-biometric-password-case Chore: Remove unused import

--- app/src/main/java/com/architectcoders/arquitectomarvel/ui/main/MainViewModel.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/ui/main/MainViewModel.kt
@@ -9,7 +9,6 @@ import com.architectcoders.arquitectomarvel.ui.main.pagination.CharacterRemoteMe
 import com.architectcoders.usecases.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
