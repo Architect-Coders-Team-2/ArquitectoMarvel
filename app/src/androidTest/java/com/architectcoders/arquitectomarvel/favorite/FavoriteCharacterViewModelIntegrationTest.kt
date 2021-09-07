c83578e90e00a48518b9856788582c64d732d175
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-09-07 21:21:15 +0200

    feature/integration-testing Test: Add FavoriteCharacterViewModelIntegrationTest

---
+++ app/src/androidTest/java/com/architectcoders/arquitectomarvel/favorite/FavoriteCharacterViewModelIntegrationTest.kt
@@ -0,0 +1,42 @@
+package com.architectcoders.arquitectomarvel.favorite
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.architectcoders.arquitectomarvel.ui.favorite.FavoriteCharacterViewModel
+import com.architectcoders.usecases.GetLocalFavoriteCharacters
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.Dispatchers
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import javax.inject.Inject
+
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+class FavoriteCharacterViewModelIntegrationTest {
+
+    @get:Rule
+    val rule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var getLocalFavoriteCharacters: GetLocalFavoriteCharacters
+
+    private lateinit var favoriteCharacterViewModel: FavoriteCharacterViewModel
+
+    @Before
+    fun setUp() {
+        rule.inject()
+        favoriteCharacterViewModel = FavoriteCharacterViewModel(
+            Dispatchers.Unconfined,
+            getLocalFavoriteCharacters
+        )
+    }
+
+    @Test
+    fun confirmIfUiModelState_isRetrieveFavoriteCharacters() {
+        assert(
+            favoriteCharacterViewModel.uiModel.value is FavoriteCharacterViewModel.UiModel.RetrieveFavoriteCharacters
+        )
+    }
+}
