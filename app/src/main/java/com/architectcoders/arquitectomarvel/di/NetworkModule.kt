37f3d8c3cc234f12e07da55437d2c181e68b9292
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-09-07 21:04:52 +0200

    feature/unit-testing Fix: Update modules to be replaced easily on tests

--- app/src/main/java/com/architectcoders/arquitectomarvel/di/NetworkModule.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/di/NetworkModule.kt
@@ -7,15 +7,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 
 @Module
 @InstallIn(ViewModelComponent::class)
-class CommonModule {
-
-    @Provides
-    fun coroutineDispatcherProvider(): CoroutineDispatcher = Dispatchers.Main
+class NetworkModule {
 
     @Provides
     fun handleNetworkManagerProvider(networkRepository: NetworkRepository): HandleNetworkManager =
