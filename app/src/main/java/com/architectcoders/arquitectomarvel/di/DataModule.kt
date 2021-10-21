021885fde7382f6aac9fbab27935e648bb03daaf
Juan Antonio <ja.cobos.obrero@gmail.com>
2021-10-21 11:42:09 +0200

    feature/improve-hilt-injection Refactor: Update modules
    Update DataModule and CharacterDetailModule to remove those functions which provide instances from classes that can be constructor injected

--- app/src/main/java/com/architectcoders/arquitectomarvel/di/DataModule.kt
+++ app/src/main/java/com/architectcoders/arquitectomarvel/di/DataModule.kt
@@ -4,44 +4,15 @@ import com.architectcoders.arquitectomarvel.data.database.RoomDataSource
 import com.architectcoders.arquitectomarvel.data.server.MarvelCredentialDataSource
 import com.architectcoders.arquitectomarvel.data.server.RetrofitDataSource
 import com.architectcoders.arquitectomarvel.network.NetworkDataSourceImpl
-import com.architectcoders.data.repository.MarvelRepository
-import com.architectcoders.data.repository.NetworkRepository
 import com.architectcoders.data.source.CredentialsDataSource
 import com.architectcoders.data.source.LocalDataSource
 import com.architectcoders.data.source.NetworkDataSource
 import com.architectcoders.data.source.RemoteDataSource
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
-
-// Providers
-
-@Module
-@InstallIn(SingletonComponent::class)
-class DataModuleProviderForMarvelRepo {
-
-    @Singleton
-    @Provides
-    fun marvelRepositoryProvider(
-        remoteDataSource: RemoteDataSource,
-        localDataSource: LocalDataSource
-    ): MarvelRepository = MarvelRepository(remoteDataSource, localDataSource)
-}
-
-@Module
-@InstallIn(SingletonComponent::class)
-class DataModuleProviderForNetwork {
-
-    @Singleton
-    @Provides
-    fun networkRepositoryProvider(networkDataSourceImpl: NetworkDataSourceImpl): NetworkRepository =
-        NetworkRepository(networkDataSourceImpl)
-}
-
-// Binders
 
 @Module
 @InstallIn(SingletonComponent::class)
